7c417fbc2fdc5c4a1fe1346712541b5357d8b6f7
Quontium <91844480+Quontium@users.noreply.github.com>
2021-10-10 01:48:11 -0500

    Delete Assets directory

--- Assets/Scripts/IController.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-public interface IController
-{
-    public event Action<Vector3> OnFlightControlInput;
-    public event Action<bool> OnThrottleInput;
-    public event Action<bool> OnBrakeInput;
-}
--- Assets/Scripts/PlacesToBeHit.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlacesToBeHit : MonoBehaviour
-{
-    [Range(-100,100)]
-    public float procentExtraDamage;
-}
